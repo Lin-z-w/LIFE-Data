78b887dfe552214b73ec1aad80837ed43a4b7d3c
Lin-z-w <2994587066@qq.com>
2025-07-11 06:31:15 +0000

    delete double delay data

--- script/plot_all.sh
+++ script/plot_all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # throughput
-python3 script/throughput.py origin_data/delay/single_delay/ --output plot/throughput/delay
+python3 script/throughput.py origin_data/delay/ --output plot/throughput/delay
 python3 script/throughput.py origin_data/throughput/congest/ --output plot/throughput/congest --no-config
 python3 script/throughput.py origin_data/throughput/rain/ --output plot/throughput/rain --no-config
 python3 script/throughput.py origin_data/throughput/random_loss/ --output plot/throughput/random_loss --no-config
-python3 script/throughput.py origin_data/throughput/reconfig\&hadover/single_delay/ --output plot/throughput/r\&r
+python3 script/throughput.py origin_data/throughput/reconfig\&hadover/ --output plot/throughput/reconfig\&hadover
 
 # delay
-python3 script/delay.py origin_data/delay/single_delay/ plot/delay/
+python3 script/delay.py origin_data/delay/ plot/delay/
 python3 script/delay.py origin_data/throughput/congest/ plot/delay/
 python3 script/delay.py origin_data/throughput/rain/ plot/delay/
 python3 script/delay.py origin_data/throughput/random_loss/ plot/delay/
-python3 script/delay.py origin_data/throughput/reconfig\&hadover/single_delay/ plot/delay/
+python3 script/delay.py origin_data/throughput/reconfig\&hadover/ plot/delay/
